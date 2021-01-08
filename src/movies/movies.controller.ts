251384d63c93c5e27934db3c44203ba3f8c7e4a9
조형호 <66339799+hyungho-hcnc@users.noreply.github.com>
2021-01-08 17:47:58 +0900

    add create-movie dto

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,7 @@
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { get } from 'http';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -14,7 +14,7 @@ export class MoviesController {
     }
 
     @Get("/:id")
-    getOne(@Param("id") movieID: string): Movie {
+    getOne(@Param("id") movieID: number): Movie {
         return this.moviesService.getOne(movieID);
     }
 
@@ -24,20 +24,17 @@ export class MoviesController {
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDto){
         return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
-    remove(@Param("id") movieID: string){
+    remove(@Param("id") movieID: number){
         return this.moviesService.deleteOne(movieID);
     }
     
     @Patch("/:id")
-    patch(@Param("id") movieId: string, @Body() updateData){
-        return {
-            updateMovie: movieId,
-            ...updateData
-        }
+    patch(@Param("id") movieId: number, @Body() updateData){
+        return this.moviesService.update(movieId, updateData);
     }
 }
