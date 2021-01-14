c4fa54243a884fd5abb8ce3b156ec9ae5abc6514
조형호 <66339799+hyungho-hcnc@users.noreply.github.com>
2021-01-14 17:33:16 +0900

    write test cases

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -12,7 +13,80 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  function createMovie() {
+    service.create({
+      title: 'Test Movie',
+      genres: ['test'],
+      year: 2000,
+    });
+  }
+
+  describe('getAll()', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      createMovie();
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('deleteOne()', () => {
+    it('deletes a movie', () => {
+      createMovie();
+      const allMoviesCount = service.getAll().length;
+      service.deleteOne(1);
+      const afterDeleteCount = service.getAll().length;
+
+      expect(afterDeleteCount).toEqual(allMoviesCount - 1);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create()', () => {
+    it('should create movie', () => {
+      const beforCreateCount = service.getAll().length;
+      createMovie();
+      const afterCreateCount = service.getAll().length;
+      expect(afterCreateCount).toEqual(beforCreateCount + 1);
+    });
+  });
+
+  describe('update()', () => {
+    it('should update a movie', () => {
+      createMovie();
+      service.update(1, { year: 2020 });
+      const afterUpdate = service.getOne(1);
+      expect(afterUpdate.year).toEqual(2020);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        createMovie();
+        service.update(1, { year: 2020 });
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
